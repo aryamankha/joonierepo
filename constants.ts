f5f8015ae3aa2976941c46b9c756d47a154fa69f
Aryaman Khandelwal <62365335+aryamankha@users.noreply.github.com>
2026-01-06 17:31:06 -0500

    Update constants.ts

--- constants.ts
+++ constants.ts
@@ -1,6 +1,6 @@
 export const SOCIALS = [
 	{ name: 'email', href: 'mailto:hello@joonie.dev' },
 	{ name: 'github', href: 'https://github.com/iteratetograceness' },
-	{ name: 'twitter', href: 'https://twitter.com/jueungraceyun' },
+	{ name: 'twitter', href: 'https://twitter.com/aryamankhawow' },
 	{ name: 'linkedin', href: 'https://www.linkedin.com/in/gracejueunyun/' }
 ] as const;
