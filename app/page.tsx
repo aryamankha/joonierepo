087e52429b6f0ce1aa3566c92765a7a402a8757b
Aryaman Khandelwal <62365335+aryamankha@users.noreply.github.com>
2026-01-09 16:03:36 -0500

    Add personal details and project links to page

--- app/page.tsx
+++ app/page.tsx
@@ -24,6 +24,8 @@ export default function Home() {
 					I am currently an engineer at Vercel. In my free time, I'm working on shipping some cool shit like {' '}
 					<Link href='/projects#dotelier'>a pixel icon studio</Link> powered by my fine-tuned flux.1
 					model and a Ricoh film simulation recipe generator.
+
+					Ary and Pran are my best friends. 
 				</p>
 			</div>
 		</div>
